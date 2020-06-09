24ebd5ae288e9bf13b3bc5a7f51fcbbb7c183cd6
Kevin Washburn <kevincwashburn@gmail.com>
2020-06-09 14:35:12 -0400

    moved MyInfo into components dir

---
+++ practice-app/src/components/MyInfo.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Kevin</h1>
+            <p>
+                I am a Junior Web Dev.
+            </p>
+            <ul>
+                <li>Playa del Carmen, Mexico</li>
+                <li>Bali, Indonesia</li>
+                <li>Banff National Park, Canada</li>
+            </ul>
+        </div>
+    )
+};
+
+export default MyInfo;
--- practice-app/src/index.js
+++ practice-app/src/index.js
@@ -1,20 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function MyInfo() {
-  return (
-    <div>
-      <h1>Kevin</h1>
-      <p>
-        I am a Junior Web Dev.
-      </p>
-      <ul>
-        <li>Playa del Carmen, Mexico</li>
-        <li>Bali, Indonesia</li>
-        <li>Banff National Park, Canada</li>
-      </ul>
-    </div>
-  )
-};
+import MyInfo from "./components/MyInfo"
 
 ReactDOM.render(<MyInfo />, document.getElementById("root"));
