838d82ef958d5629f7c377c44f4fcf0033bc9532
Kevin Washburn <kevincwashburn@gmail.com>
2020-06-09 14:46:58 -0400

    added footer element

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <h1>Hello World!</h1>
+        <ul>
+          <li>thing 1</li>
+          <li>thing 2</li>
+          <li>thing 3</li>
+        </ul>
+      </nav>
+      <main>
+        <p>Here is a paragraph in the main section.</p>
+      </main>
+      <Footer />
     </div>
-  );
-}
+  )
+};
 
 export default App;
