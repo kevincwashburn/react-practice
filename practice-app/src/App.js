8304b1245121af58ab92e6b220639cfc5d9f2b62
Kevin Washburn <kevincwashburn@gmail.com>
2020-06-10 14:17:07 -0400

    added timeOfDay logic to App.js

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -4,9 +4,22 @@ import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 
 function App() {
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+  
+  if(hours < 12) {
+    timeOfDay = "morning"
+  } else if (hours > 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else {
+    timeOfDay = "night"
+  }
+
   return (
     <div>
       <Header />
+      <h3>Good {timeOfDay}!</h3>
       <MainContent />
       <Footer />
     </div>
