a28d9a35ad63ed332cf7eb55a2643d89a22d931a
Kevin Washburn <kevincwashburn@gmail.com>
2020-06-11 15:21:11 -0400

    props example

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,35 +1,36 @@
 import React from "react";
-// import Header from "./components/Header";
-// import MainContent from "./components/MainContent";
-// import Footer from "./components/Footer";
-import TodoItem from "./components/TodoItem";
+import ContactCard from "./ContactCard";
 
 function App() {
-  const date = new Date();
-  const hours = date.getHours();
-  let timeOfDay;
-  const styles = {
-    fontSize: 30,
-    fontFamily: "Roboto, sans-serif",
-    textAlign: "center"
-  };
-
-  if (hours < 12) {
-    timeOfDay = "morning";
-  } else if (hours > 12 && hours < 17) {
-    timeOfDay = "afternoon";
-  } else {
-    timeOfDay = "night";
-  };
-
   return (
-    <div>
-      <h3 style={styles}>Good {timeOfDay}!</h3>
-      <div className="todo-list">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-      </div>
+    <div className="contacts">
+      <ContactCard 
+          name="Mr. Whiskerson"
+          imgUrl="http://placekitten.com/300/200"
+          phone="(212) 555-1234"
+          email="mr.whiskaz@catnap.meow"
+      />
+      <ContactCard
+        name="Fluffykins"
+        imgUrl="http://placekitten.com/400/200"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
+      />
+      <ContactCard
+        name="Destroyer"
+        imgUrl="http://placekitten.com/400/300"
+        phone="(212) 555-3456"
+        email="ofworlds@yahoo.com"
+      />
+      <ContactCard
+        name="Felix"
+        imgUrl="http://placekitten.com/200/100"
+        phone="(212) 555-4567"
+        email="thecat@homtail.com"
+      />
+
+      
+
     </div>
   )
 };
