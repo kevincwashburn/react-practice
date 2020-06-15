499ddab85505ccdee095e9234b34f401fe36b554
Kevin Washburn <kevincwashburn@gmail.com>
2020-06-15 13:05:22 -0400

    added Joke.js review exercise

--- practice-app/src/Joke.js
+++ practice-app/src/Joke.js
@@ -3,8 +3,8 @@ import React from "react";
 function Joke(props) {
     return (
         <div className="joke-card">
-            <h3>{props.question}</h3>
-            <p>{props.punchLine}</p>
+            <h3>Question: {props.question}</h3>
+            <p>Answer: {props.punchLine}</p>
         </div>
     )
 };
