631a329103bd4fd0c928ece2ecfe99f2bbd79f1a
Kevin Washburn <kevincwashburn@gmail.com>
2020-06-15 13:11:06 -0400

    added ternary operator for Question display in Joke.js

--- practice-app/src/Joke.js
+++ practice-app/src/Joke.js
@@ -3,7 +3,7 @@ import React from "react";
 function Joke(props) {
     return (
         <div className="joke-card">
-            <h3>Question: {props.question}</h3>
+            <h3 style={{display: props.questions ? "block" : "none"}}>Question: {props.question}</h3>
             <p>Answer: {props.punchLine}</p>
         </div>
     )
