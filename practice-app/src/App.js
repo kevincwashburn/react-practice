c0f0390b80e114f66cc5945d587e31af44b587e2
Kevin Washburn <kevincwashburn@gmail.com>
2020-06-15 13:32:42 -0400

    added jokesData.js and logic in App.js

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
 import Joke from "./Joke";
+import jokesData from "./jokesData";
 
 function App() {
+
+ const jokeComponents = jokesData.map(joke => {
+  return (
+    <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />
+  )
+ });
+
   return (
     <div>
-      <Joke
-      question="Why did the chicken cross the road?"
-      punchLine="To get to the other side."
-      />
-      <Joke
-      question="Why do they tell actors to break a leg?"
-      punchLine="Because every play has a cast."
-      />
-      <Joke
-      question="Helvetica and Times New Roman walk into a bar."
-      punchLine="Get out of here! We don't serve your type!"
-      />
+     {jokeComponents}
     </div>
   )
 };
