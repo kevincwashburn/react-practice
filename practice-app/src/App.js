42a7d1c63437c69872be0be940fe7fb435049834
Kevin Washburn <kevincwashburn@gmail.com>
2020-06-15 18:53:38 -0400

    completed practice exercise

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -3,13 +3,11 @@ import Product from "./Product";
 import productsData from "./productsData";
 
 function App() {
-  const productsComponents = productsData.map(item => 
-    <Product key={item.id} product={item} />
-    )
+  const productCompilation = productsData.map(item => <Product key={item.id} product={item}/>);
 
   return (
     <div>
-      {productsComponents}
+    {productCompilation}
     </div>
   )
 };
