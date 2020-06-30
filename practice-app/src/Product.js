49c614dfeb818cf13414671463287f502e7d18bb
Kevin Washburn <kevincwashburn@gmail.com>
2020-06-30 15:17:23 -0400

    re-did Product.js for practice

--- practice-app/src/Product.js
+++ practice-app/src/Product.js
@@ -4,10 +4,10 @@ function Product(props) {
     return (
         <div>
             <h2>{props.product.name}</h2>
-            <h2>{props.product.price.toLocaleString("en-US", {style: "currency", currency: "USD"})}</h2>
-            <p>{props.product.description}</p>
+            <p>{props.product.price.toLocaleString("en-US", {style: "currency", currency: "USD" })}
+            - {props.product.description}</p>
         </div>
     )
-};
+}
 
 export default Product;
