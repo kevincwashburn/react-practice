7714cb5109fa30efa6a6d31e4248bb651a644e7a
Kevin Washburn <kevincwashburn@gmail.com>
2020-06-30 15:34:18 -0400

    revised TodoItem.js for current exercise

--- practice-app/src/TodoItem.js
+++ practice-app/src/TodoItem.js
@@ -4,10 +4,8 @@ import "../style.css";
 function TodoItem() {
     return (
         <div className="todo-item">
-            <input type="checkbox" id="box1"></input>
-            <span> Placeholder text</span>
-            <br></br>
-            <br></br>
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
         </div>
     )
 };
