08d835fbb75700e9f92f3fd92df41e63158e2fc9
Kevin Washburn <kevincwashburn@gmail.com>
2020-07-01 15:58:46 -0400

    Changing State Exercise Example in App.js

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div>
-      <img src="https://www.fillmurray.com/200/100" alt="placeholder" onMouseOver={() => console.log("I was hovered over")
-      } />
-      <br />
-      <br />
-      <button onClick={() => console.log("I was clicked!")
-      }>Click Me</button>
-    </div>
-  )
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleClick}>Change!</button>
+      </div>
+    )
+  }
 };
 
 export default App;
-
