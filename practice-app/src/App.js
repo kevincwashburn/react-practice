d41292620d37d1a2a017c4c3283bcf8ac2ee464e
Kevin Washburn <kevincwashburn@gmail.com>
2020-07-01 12:37:45 -0400

    more state/constructor practice

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,22 +1,24 @@
-import React from "react";
+// pre-answer 2:12:26
+// answer 2:15:11
 
-class App extends React.Component {
+import React, {Component} from "react";
+
+class App extends Component {
   constructor() {
-  super()
-  this.state = {
-    name: "Kevin",
-    age: 27
+    super()
+    this.state = {
+      name: "Marcos",
+      age: 27
+    }
   }
-}
   render() {
     return (
       <div>
         <h1>{this.state.name}</h1>
-        <h3>{this.state.age} years old</h3>
+        <h3>{this.state.age}</h3>
       </div>
     )
   }
-  
 }
 
 export default App;
