f13db0a47388dc2d8d900313459c2f9e733411dc
Kevin Washburn <kevincwashburn@gmail.com>
2020-07-01 12:08:54 -0400

    Class-based component exercise answers in index.js

--- practice-app/src/index.js
+++ practice-app/src/index.js
@@ -1,32 +1,50 @@
-// 2:04:11
+// 2:04:11 before answers
+// 2:07:10 after answers
 
 import React from "react";
 import ReactDOM from "react-dom";
 
-//#1
-function App() {
-    return (
-        <div>
-            <Header />
-            <Greeting />
-        </div>
-    )
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header username="Kevin" />
+                <Greeting />
+            </div>
+        )
+    }
+};
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <p>Welcome, {this.props.username}</p>
+            </header>
+        )
+    }
 };
 
 
+class Greeting extends React.Component {
+      render() {
+          const date = new Date();
+          const hours = date.getHours();
+          let timeOfDay;
 
-//#2
-function Header(props) {
-    return (
-        <header>
-            <p>Welcome, {props.username}!</p>
-        </header>
-    )
-};
+          if (hours < 12) {
+              timeOfDay = "morning"
+          } else if (hours >= 12 && hours < 17) {
+              timeOfDay = "afternoon"
+          } else {
+              timeOfDay = "night"
+          }
 
-//#3
-function Greeting() {
-    const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay;
-};
+          return (
+              <h1>Good {timeOfDay} to you, sir!</h1>
+          )
+      }
+}
+
+
+ReactDOM.render(<App/>, document.getElementById("root"));
