485cdd46f3ddc73c24b12bf501598032100e0cf5
kevincwashburn <kevincwashburn@gmail.com>
2020-07-10 16:20:30 -0400

    Conditional Rendering Practice Exercise Solved - App.js

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,20 +1,29 @@
 import React, {Component} from "react";
-// import Conditional from "./Conditional";
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      unreadMessages: ["test"]
+      isLoggedIn: false
     }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState(prevState => {
+      return {
+        isLoggedIn: !prevState.isLoggedIn
+      }
+    })
   }
   render() {
+    let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN";
+    let displayText = this.state.isLoggedIn ? "Logged In" : "Logged Out"
+
     return (
       <div>
-        {
-          this.state.unreadMessages.length > 0 &&
-          <h2>You have {this.state.unreadMessages.length} unread messages!</h2>
-        }
+        <button onClick={this.handleClick}>{buttonText}</button>
+        <h2>{displayText}</h2>
       </div>
     )
   }
