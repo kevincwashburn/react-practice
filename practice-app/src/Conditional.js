7baa2f4e961d11b628e3dd062212910a802c8beb
kevincwashburn <kevincwashburn@gmail.com>
2020-07-10 15:38:12 -0400

    replaced logic with ternary operator in Conditional.js

--- practice-app/src/Conditional.js
+++ practice-app/src/Conditional.js
@@ -2,15 +2,12 @@ import React from "react";
 
 function Conditional(props) {
     // condition ? statement if true : statement if false
-    if(props.isLoading === true) {
-        return (
-            <h1>Loading...</h1>
-        )
-    } else {
-        return (
-            <h1>Not loading.</h1>
-        )
-    }
+   
+    return (
+        <div>
+            {props.isLoading === true ? <h1>Loading...</h1> : <h1>Not loading.</h1>}
+        </div>
+    )
 }
 
 export default Conditional;
