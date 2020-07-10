0ff41d50ed2d6753b50adeb511f3a167a60d9ca9
kevincwashburn <kevincwashburn@gmail.com>
2020-07-10 10:59:31 -0400

    added legacy methods for comprehension (with notes) in App.js - lifecycle methods part 1

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -6,6 +6,23 @@ class App extends Component {
     this.state = {}
   }
 
+ 
+  componentDidMount() {
+ // this method will only run once, while component is displayed on the screen.
+  // common uses:
+  // - API calls (data retreival)
+  }
+
+  // DEPREICATED...
+  componentWillReceiveProps(nextProps) {
+    // this component could be receiving props from a parent component
+    // every time this component receives props, this method runs
+    // will not only run every time the component is mounted, but will also run every time a parent component hands props to child component
+    if (nextProps.whatever !== this.props.whatever) {
+      // do something important here
+    }
+  }
+
   render() {
     return (
       <div>
