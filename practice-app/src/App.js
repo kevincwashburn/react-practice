e2d8794c1d95092d0973695ba37fb2b62cb3f9cb
kevincwashburn <kevincwashburn@gmail.com>
2020-07-09 20:12:01 -0400

    changed completed: true to false on all objects in todosData.js

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
 
   handleChange(id) {
     this.setState(prevState => {
-      const updatedTodos = prevState.map(todo => {
+      const updatedTodos = prevState.todos.map(todo => {
         if (todo.id === id) {
           todo.completed = !todo.completed
         }
